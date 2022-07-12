ae0440c46782b4eb7253e42ffa52ca6e026497ec
woodonghun <ehcldnehd@naver.com>
2022-07-12 16:33:58 +0900

    edt, label

--- UI/dlgSign.py
+++ UI/dlgSign.py
@@ -14,29 +14,29 @@ class DlgSign(QWidget):
         self.dialog_close()
 
     def sign(self):
-        ok = QPushButton('ok', self.dialog)
-        ok.move(20, 120)
-        ok.clicked.connect(self.signup)  # OK 버튼 눌렀을 때 회원가입
+        btn_Ok = QPushButton('ok', self.dialog)
+        btn_Ok.move(20, 120)
+        btn_Ok.clicked.connect(self.signup)  # OK 버튼 눌렀을 때 회원가입
 
-        cancel = QPushButton('cancel', self.dialog)
-        cancel.move(100, 120)
-        cancel.clicked.connect(self.dialog_close)
+        btn_Cancel = QPushButton('cancel', self.dialog)
+        btn_Cancel.move(100, 120)
+        btn_Cancel.clicked.connect(self.dialog_close)
 
-        ID_dialog_Label = QLabel("ID", self.dialog)
-        ID_dialog_Label.move(20, 35)
-        PW_dialog_Label = QLabel("PW", self.dialog)
-        PW_dialog_Label.move(20, 65)
-        name_dialog_Label = QLabel("name", self.dialog)
-        name_dialog_Label.move(20, 95)
+        label_Id_Dialog = QLabel("ID", self.dialog)
+        label_Id_Dialog.move(20, 35)
+        label_Pw_Dialog = QLabel("PW", self.dialog)
+        label_Pw_Dialog.move(20, 65)
+        label_Name_Dialog = QLabel("name", self.dialog)
+        label_Name_Dialog.move(20, 95)
 
-        self.ID_dialog_line = QLineEdit(self.dialog)
-        self.ID_dialog_line.move(60, 30)
+        self.edt_Id_Dialog = QLineEdit(self.dialog)
+        self.edt_Id_Dialog.move(60, 30)
 
-        self.PW_dialog_line = QLineEdit(self.dialog)
-        self.PW_dialog_line.move(60, 60)
+        self.edt_Pw_Dialog = QLineEdit(self.dialog)
+        self.edt_Pw_Dialog.move(60, 60)
 
-        self.name_dialog_line = QLineEdit(self.dialog)
-        self.name_dialog_line.move(60, 90)
+        self.edt_Name_Dialog = QLineEdit(self.dialog)
+        self.edt_Name_Dialog.move(60, 90)
 
         self.dialog.exec()  # show 대신 exec 를 사용 하면 modal 로 동작
 
@@ -46,7 +46,8 @@ class DlgSign(QWidget):
         self.privacy = self.privacy.replace('\n', ',')
         self.privacy_list = self.privacy.split(',')
         self.privacy_chunk = [self.privacy_list[i * 3:(i + 1) * 3] for i in range((len(self.privacy_list) + 3 - 1) // 3)]
-        print(self.privacy_chunk)
+        # 리스트 분할 https://jsikim1.tistory.com/141 참고
+
         Id = self.ID_dialog_line.text()
         pw = self.PW_dialog_line.text()
         name = self.name_dialog_line.text()
@@ -66,7 +67,6 @@ class DlgSign(QWidget):
                 txt.write("\n")
                 UI.signBox.SignBox('회원 가입이 완료 되었습니다.')
                 self.dialog_close()
-
 
         elif Id != '' or pw != '' or name != '':  # 하나 라도 빈칸 있을때
             UI.signBox.SignBox('빈칸 없이 다시 입력 하세요.')
